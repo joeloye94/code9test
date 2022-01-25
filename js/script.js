1116c6d82f59471f16f5d81c96921efce1ac8ef3
Joel Oye <joel.oye94@gmail.com>
2022-01-25 16:07:39 +0800

    add navbar, more changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(function(){
 
-    console.log(123)
+    
+    //nav
+    navbarInit();
+    function navbarInit(){
+
+        $(".nav__tab:eq(0)").addClass('nav__tab--active');
+
+        $(".nav__tab").click(function(e){
+            var $target = e.target.classList.value.indexOf('nav__tab') > -1 ? $(e.target) : $(e.target).closest('.nav__tab');
+            if(!$target.hasClass('nav__tab--active')) {
+                $(".nav__tab.nav__tab--active").removeClass('nav__tab--active');
+                $target.addClass('nav__tab--active')
+            }
+
+        })
+    }
 })
