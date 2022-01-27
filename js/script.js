1b9df937b743145cc0f9e7c9eeeebac3e5bf5871
Joel Oye <joel.oye94@gmail.com>
2022-01-27 11:14:11 +0800

    fix ie tab not working

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(function(){
         $('.maincontent').eq(0).show();
         
         $(".nav__tab").click(function(e){
-            var $target = e.target.classList.value.indexOf('nav__tab') > -1 ? $(e.target) : $(e.target).closest('.nav__tab');
+            var $target = e.target.className.indexOf('nav__tab') > -1 ? $(e.target) : $(e.target).closest('.nav__tab');
             if(!$target.hasClass('nav__tab--active')) {
                 $(".nav__tab.nav__tab--active").removeClass('nav__tab--active');
                 $target.addClass('nav__tab--active')
@@ -51,7 +51,7 @@ $(function(){
         var checkedIn = true;
         
         $(".safeentry__tab").click(function(e){
-            var $target = e.target.classList.value.indexOf('safeentry__tab') > -1 ? $(e.target) : $(e.target).closest('.safeentry__tab');
+            var $target = e.target.className.indexOf('safeentry__tab') > -1 ? $(e.target) : $(e.target).closest('.safeentry__tab');
             if(!$target.hasClass('safeentry__tab--active')) {
                 $(".safeentry__tab--active").removeClass('safeentry__tab--active');
                 $target.addClass('safeentry__tab--active')
